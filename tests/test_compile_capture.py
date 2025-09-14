d556f313942a0c23c0fbfb04b550361198a99ffa
littlecube8152 <froakie20161113@gmail.com>
2025-09-14 21:24:20 +0800

    Attempt to fix previous test

--- tests/test_compile_capture.py
+++ tests/test_compile_capture.py
@@ -7,6 +7,11 @@ from internal.steps.generation import GenerationStep
 from internal.commands import command_clean
 
 
+# run compilation in another thread
+def _compile(generation_step: GenerationStep):
+    generation_step.prepare_sandbox()
+    generation_step.compile()
+
 def test_compile_capture():
     script_dir = pathlib.Path(__file__).parent.parent.resolve()
     problem_dir = pathlib.Path(__file__).parent.resolve() / "bad-generator"
@@ -15,17 +20,11 @@ def test_compile_capture():
 
     command_clean(formatter=formatter, context=context, skip_confirm=True)
 
+    context.config.trusted_compile_time_limit_sec = 10 # overrides
     generation_step = GenerationStep(context)
 
     import multiprocessing
-
-    context.config.trusted_compile_time_limit_sec = 10 # overrides
-    # run compilation in another thread
-    def compile():
-        generation_step.prepare_sandbox()
-        generation_step.compile()
-
-    p = multiprocessing.Process(target=compile)
+    p = multiprocessing.Process(target=_compile, args=(generation_step,))
     p.start()
 
     # Wait for 15 seconds or until process finishes
