2d653a93434c583c354dbc695a3556f18bdbeddd
littlecube8152 <froakie20161113@gmail.com>
2026-02-25 23:36:52 +0800

    Refactor guess interactor

--- examples/icpc/guess/interactor/checker.cc
+++ examples/icpc/guess/interactor/checker.cc
@@ -9,37 +9,28 @@
 const int EXIT_AC = 42;
 const int EXIT_WA = 43;
 
-FILE *judgemessage = NULL;
-
-void wrong_answer(const char *err)
-{
-    fprintf(judgemessage, "%s\n", err);
-    exit(EXIT_WA);
-}
-
-FILE *openfeedback(const char *feedbackdir, const char *feedback)
-{
-    std::string path = std::string(feedbackdir) + "/" + std::string(feedback);
-    FILE *res = fopen(path.c_str(), "w");
-    if (!res)
-        std::abort();
-    return res;
-}
-
-const char *USAGE = "Usage: %s judge_in judge_ans feedback_file [options] < user_out";
-
-static const int max_queries = 11;
+static const int MAX_QUERIES = 11;
 
 int main(int argc, char **argv)
 {
     if (argc < 4)
+    {
+        std::cout << "Usage: %s judge_in judge_ans feedback_dir [options] < user_out";
+        std::abort();
+    }
+
+    std::ifstream judgein(argv[1]);
+    std::ifstream judgeans(argv[2]);
+    std::ofstream judgemessage(std::string(argv[3]) + "judgemessage.txt");
+
+    if (judgein.fail() || judgeans.fail() || judgemessage.fail())
         std::abort();
 
-    judgemessage = openfeedback(argv[3], "judgemessage.txt");
-
-    std::ifstream judgein(argv[1]), judgeans(argv[2]);
-    if (judgein.fail() || judgeans.fail())
-        std::abort();
+    auto wrong_answer = [&judgemessage](const char *err)
+    {
+        judgemessage << err << '\n';
+        exit(EXIT_WA);
+    };
 
     std::string mode;
     judgein >> mode;
@@ -51,17 +42,17 @@ int main(int argc, char **argv)
         judgein >> answer;
 
         int guess;
-        while (++queries <= max_queries && std::cin >> guess)
+        while (++queries <= MAX_QUERIES && std::cin >> guess)
         {
             if (guess == answer)
             {
                 std::cout << "=" << std::endl;
                 break;
             }
-            else if (guess < answer)
-                std::cout << ">" << std::endl;
-            else
+            else if (answer < guess)
                 std::cout << "<" << std::endl;
+            else
+                std::cout << ">" << std::endl;
             std::cerr << "received guess " << guess << '\n';
         }
     }
@@ -69,16 +60,16 @@ int main(int argc, char **argv)
     {
         int l = 1, r = 1024;
         int guess;
-        while (++queries <= max_queries && std::cin >> guess)
+        while (++queries <= MAX_QUERIES && std::cin >> guess)
         {
             if (guess == l && guess == r)
             {
                 std::cout << "=" << std::endl;
                 break;
             }
-            else if (guess < l)
+            else if (l > guess)
                 std::cout << ">" << std::endl;
-            else if (guess > r)
+            else if (r < guess)
                 std::cout << "<" << std::endl;
             else
             {
@@ -87,24 +78,25 @@ int main(int argc, char **argv)
                     l = guess + 1;
                 else
                     r = guess - 1;
-                if (guess < l)
+                if (l > guess)
                     std::cout << ">" << std::endl;
-                else if (guess > r)
+                else if (r < guess)
                     std::cout << "<" << std::endl;
                 else
-                    assert(false);
+                    std::abort(); // should be impossible
             }
         }
     }
     else
-        assert(false);
+        std::abort(); // should be impossible
 
-    if (queries > max_queries)
+    if (queries > MAX_QUERIES)
     {
         std::cout << "-" << std::endl;
-        wrong_answer(("Participant exceeded maximum queries count " + std::to_string(max_queries)).c_str());
+        wrong_answer(("Participant exceeded maximum queries count " + std::to_string(MAX_QUERIES)).c_str());
     }
     if (std::cin.fail())
         wrong_answer("Failed to read integer from the participant");
+
     exit(EXIT_AC);
 }
