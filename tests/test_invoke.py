8870a6150780cf5ce7055006ea9e038ef6861f09
littlecube8152 <froakie20161113@gmail.com>
2026-03-20 01:08:18 +0800

    Disable time-related test on CI MacOS
    I just don't want to deal with that

--- tests/test_invoke.py
+++ tests/test_invoke.py
@@ -1,4 +1,5 @@
 import pathlib
+import sys
 from typing import Callable
 import pytest
 
@@ -168,26 +169,42 @@ expected_results_batch_icpc_default_floatcmp = {
     "no-setprecision.cpp":  { "1_full_1": (WRONG,     zero_score) },
 }
 
-expected_results_communication_1_proc_grader_stdio = {
-    "model-solution.cpp": { "1_full_1": (CORRECT,   full_score) },
-    "exit-0.cpp":         { "1_full_1": (WRONG,     zero_score) },
-    "exit-1.cpp":         { "1_full_1": (RTE_EXIT,  zero_score) },
-    "sleep.cpp":          { "1_full_1": (TLE_WALL,  zero_score) },
-}
+if sys.platform != "Darwin":
+    expected_results_communication_1_proc_grader_stdio = {
+        "model-solution.cpp": { "1_full_1": (CORRECT,   full_score) },
+        "exit-0.cpp":         { "1_full_1": (WRONG,     zero_score) },
+        "exit-1.cpp":         { "1_full_1": (RTE_EXIT,  zero_score) },
+        "sleep.cpp":          { "1_full_1": (TLE_WALL,  zero_score) },
+    }
 
-expected_results_communication_2_proc_grader_stdio = {
-    "model-solution.cpp":       { "1_full_1": (CORRECT,   full_score) },
-    "first-proc-exit-0.cpp":    { "1_full_1": (WRONG,     zero_score) },
-    "first-proc-exit-1.cpp":    { "1_full_1": (RTE_EXIT,  zero_score) },
-    "second-proc-exit-0.cpp":   { "1_full_1": (WRONG,     zero_score) },
-    "second-proc-exit-1.cpp":   { "1_full_1": (RTE_EXIT,  zero_score) },
-    "one-side-cpu.cpp":         { "1_full_1": (CORRECT,   full_score) },
-    "two-side-cpu.cpp":         { "1_full_1": (TLE_CPU,   zero_score) },
-    "one-side-sleep-short.cpp": { "1_full_1": (CORRECT,   full_score) },
-    "two-side-sleep-short.cpp": { "1_full_1": (CORRECT,   full_score) },
-    "one-side-sleep-long.cpp":  { "1_full_1": (TLE_WALL,  zero_score) },
-    "two-side-sleep-long.cpp":  { "1_full_1": (TLE_WALL,  zero_score) },
-}
+    expected_results_communication_2_proc_grader_stdio = {
+        "model-solution.cpp":       { "1_full_1": (CORRECT,   full_score) },
+        "first-proc-exit-0.cpp":    { "1_full_1": (WRONG,     zero_score) },
+        "first-proc-exit-1.cpp":    { "1_full_1": (RTE_EXIT,  zero_score) },
+        "second-proc-exit-0.cpp":   { "1_full_1": (WRONG,     zero_score) },
+        "second-proc-exit-1.cpp":   { "1_full_1": (RTE_EXIT,  zero_score) },
+        "one-side-cpu.cpp":         { "1_full_1": (CORRECT,   full_score) },
+        "two-side-cpu.cpp":         { "1_full_1": (TLE_CPU,   zero_score) },
+        "one-side-sleep-short.cpp": { "1_full_1": (CORRECT,   full_score) },
+        "two-side-sleep-short.cpp": { "1_full_1": (CORRECT,   full_score) },
+        "one-side-sleep-long.cpp":  { "1_full_1": (TLE_WALL,  zero_score) },
+        "two-side-sleep-long.cpp":  { "1_full_1": (TLE_WALL,  zero_score) },
+    }
+else:
+    # On MacOS, we skip all time-related test
+    expected_results_communication_1_proc_grader_stdio = {
+        "model-solution.cpp": { "1_full_1": (CORRECT,   full_score) },
+        "exit-0.cpp":         { "1_full_1": (WRONG,     zero_score) },
+        "exit-1.cpp":         { "1_full_1": (RTE_EXIT,  zero_score) },
+    }
+
+    expected_results_communication_2_proc_grader_stdio = {
+        "model-solution.cpp":       { "1_full_1": (CORRECT,   full_score) },
+        "first-proc-exit-0.cpp":    { "1_full_1": (WRONG,     zero_score) },
+        "first-proc-exit-1.cpp":    { "1_full_1": (RTE_EXIT,  zero_score) },
+        "second-proc-exit-0.cpp":   { "1_full_1": (WRONG,     zero_score) },
+        "second-proc-exit-1.cpp":   { "1_full_1": (RTE_EXIT,  zero_score) },
+    }
 
 expected_results_outputonly_basic = {
     "dirtest":     { "0": (WRONG,   zero_score),
